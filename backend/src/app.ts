fd22626ad407eaee366951da81938962875a8c8c
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-06-21 23:42:51 +0530

    feat(app): register error and not found middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,8 @@ import cors from 'cors';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import healthRoute from './routes/health.route.js';
+import { notFoundMiddleware } from './middlewares/not-found.middleware.js';
+import { errorMiddleware } from './middlewares/error.middleware.js';
 
 const app = express();
 
@@ -14,5 +16,9 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use('/health', healthRoute);
+
+app.use(notFoundMiddleware);
+
+app.use(errorMiddleware);
 
 export default app;

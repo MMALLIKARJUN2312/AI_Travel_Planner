3b9c6687426d12a00376353c356e5057164886d9
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-06-23 02:16:57 +0530

    refactor(users): extend CreateUserDto to accept role property

--- backend/src/modules/users/repositories/user.repository.ts
+++ backend/src/modules/users/repositories/user.repository.ts
@@ -1,8 +1,11 @@
 import { UserModel } from "../models/user.model.js";
 import { z } from "zod";
 import { registerSchema } from "../../auth/schemas/register.schema.js";
+import { UserRole } from "../types/user-role.enum.js";
 
-type CreateUserDto = z.infer<typeof registerSchema>;
+type CreateUserDto = z.infer<typeof registerSchema> & {
+    role ?: UserRole;
+};
 
 export class UserRepository {
     async findByEmail(email : string) {
@@ -27,30 +30,31 @@ export class UserRepository {
             .exec();
     }
 
-    async addRefreshToken(userId : string, token : string) {
-        return UserModel.findByIdAndUpdate(userId, 
-            { $push : {refreshTokens : token}},
-            { new : true}
-        ).exec()
+    async addRefreshToken(userId: string, token: string) {
+        return UserModel.findByIdAndUpdate(
+            userId,
+            { $push: { refreshTokens: { token } } }, 
+            { new: true }
+        ).exec();
     }
 
-    async removeRefreshToken(userId : string, token : string) {
-        return UserModel.findByIdAndUpdate(userId, 
-            { $pull : {refreshTokens : token}},
-            { new : true}
-        ).exec()
+    async removeRefreshToken(userId: string, token: string) {
+        return UserModel.findByIdAndUpdate(
+            userId,
+            { $pull: { refreshTokens: { token: token } } },
+            { new: true }
+        ).exec();
     }
 
-    async replaceRefreshToken(userId : string, oldToken : string, newToken : string) {
-        return UserModel.findByIdAndUpdate(userId,
+    async replaceRefreshToken(userId: string, oldToken: string, newToken: string) {
+        return UserModel.findByIdAndUpdate(
+            userId,
             {
-                $pull : {refreshTokens : oldToken},
-                $push : {refreshTokens : newToken}
-            }, 
-            {
-                new : true
-            } 
-        ).exec()
+                $pull: { refreshTokens: { token: oldToken } },
+                $push: { refreshTokens: { token: newToken } }
+            },
+            { new: true }
+        ).exec();
     }
 }
 

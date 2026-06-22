b5cd9dfa31091ea14f9c559b5a64a46caeaeec3a
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-06-22 15:44:25 +0530

    feat(api): enhance health check with database status

--- backend/src/routes/health.route.ts
+++ backend/src/routes/health.route.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
+import mongoose from 'mongoose';
 
 const router = Router();
+
+const states = {
+    0 : "disconnected",
+    1 : "connected",
+    2 : "connecting",
+    3 : "disconnecting"
+}
 
 router.get('/', (_, res) => {
     res.status(200).json({
         success : true,
-        message : "API Healthy"
+        database : states[mongoose.connection.readyState as 0 | 1 | 2 | 3],
+        uptime : Math.floor(process.uptime())
     })
 })
 

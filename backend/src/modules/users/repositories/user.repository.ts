957a8009843f447df56baab0dfb716dff97dca0e
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-06-22 20:25:22 +0530

    feat(users): add user repository layer

---
+++ backend/src/modules/users/repositories/user.repository.ts
@@ -0,0 +1,25 @@
+import { UserModel } from "../models/user.model.js";
+import { z } from "zod";
+import { registerSchema } from "../../auth/schemas/register.schema.js";
+
+type CreateUserDto = z.infer<typeof registerSchema>;
+
+export class UserRepository {
+    async findByEmail(email : string) {
+        return UserModel.findOne({email}).exec();
+    }
+
+    async findById(id: string) {
+        return UserModel.findById(id).exec();
+    }
+
+    async create(userDto : CreateUserDto) {
+        return UserModel.create(userDto);
+    }
+
+    async save(user : any) {
+        return user.save();
+    }
+}
+
+export const userRepository = new UserRepository();

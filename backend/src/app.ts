f387b1e89bfd8c035d4ade8f2edf0a4de0979d3b
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-06-23 09:42:48 +0530

    feat(app): register authentication routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import healthRoute from './routes/health.route.js';
+import authRoutes from './modules/auth/routes/auth.routes.js'
 import { notFoundMiddleware } from './middlewares/not-found.middleware.js';
 import { errorMiddleware } from './middlewares/error.middleware.js';
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use('/health', healthRoute);
+app.use('/api/auth', authRoutes);
 
 app.use(notFoundMiddleware);
 

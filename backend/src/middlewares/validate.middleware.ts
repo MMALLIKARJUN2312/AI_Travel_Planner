77b605900d0c2ff0ca18d044606493ad548a94ed
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-06-23 19:00:43 +0530

    fix(validate): make the response user friendly

--- backend/src/middlewares/validate.middleware.ts
+++ backend/src/middlewares/validate.middleware.ts
@@ -11,10 +11,10 @@ export const validate = (schema : ZodType) => (
 
     if (!result.success) {
         const errorMessages = result.error.issues
-        .map((issue) => `${issue.path.join(".")} : ${issue.message}`)
+        .map((issue) => `(${issue.path.join(".")}) ${issue.message}`)
         .join(". ")
 
-        return next(new AppError(`Validation failed : ${errorMessages}`, 400))
+        return next(new AppError(`Validation failed, ${errorMessages}`, 400))
     }
 
     req.body = result.data;

52ae01492a5d428a4aff66f305132b44be117c4e
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-06-24 17:38:21 +0530

    feat(trips): implement repository layer

---
+++ backend/src/modules/trips/repositories/trip.repository.ts
@@ -0,0 +1,25 @@
+import { TripModel } from "../models/trip.model.js";
+import { createTripDto } from "../schemas/create-trip.schema.js";
+import { UpdateTripDto } from "../schemas/update-trip.schema.js";
+
+export class TripRepository {
+    async create(data : createTripDto & {userId : string}) {
+        return TripModel.create(data);
+    }
+
+    async findAllByUserId(userId : string) {
+        return TripModel.find({userId}).sort({createdAt : -1});
+    }
+
+    async findByIdAndUserId(tripId : string, userId : string) {
+        return TripModel.findOne({_id : tripId, userId})
+    }
+
+    async updateByIdAndUserId(tripId : string, userId : string, data : UpdateTripDto) {
+        return TripModel.findOneAndUpdate({_id : tripId, userId}, data, {new : true})
+    }
+
+    async deleteByIdAndUserId(tripId : string, userId : string) {
+        return TripModel.findOneAndDelete({_id : tripId, userId})
+    }
+}                                                                                                   

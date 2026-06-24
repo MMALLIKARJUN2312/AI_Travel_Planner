ba7ec3155c4c77793de7d7171a093c2da840bd55
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-06-24 17:15:03 +0530

    feat(trips): add trip request validation schemas

---
+++ backend/src/modules/trips/schemas/create-trip.schema.ts
@@ -0,0 +1,27 @@
+import {z} from "zod";
+import { BudgetType } from "../types/budget-type.enum.js";
+
+export const createTripSchema = z.object({
+    destination : z
+        .string() 
+        .trim()
+        .min(2)
+        .max(100),
+    numberOfDays : z 
+        .number()
+        .int()
+        .min(1)
+        .max(50),
+    budgetType : z.enum(
+        Object.values(BudgetType) as [
+            string,
+            ...string[]
+        ]
+    ),
+    interests : z
+        .array(z.string().trim())
+        .min(1)
+        .max(20)
+})
+
+export type createTripDto = z.infer<typeof createTripSchema>
---
+++ backend/src/modules/trips/schemas/update-trip.schema.ts
@@ -0,0 +1,28 @@
+import {z} from 'zod';
+import { BudgetType } from '../types/budget-type.enum.js';
+
+export const updateTripSchema = z.object({
+    destination : z 
+        .string()
+        .trim()
+        .min(2)
+        .max(100)
+        .optional(),
+
+    numberOfDays : z 
+        .number()
+        .int()
+        .min(1)
+        .max(30)
+        .optional(),
+
+    budgetType : z.enum(
+        Object.values(BudgetType) as [string, ...string[]])
+        .optional(),
+    
+    interests : z
+        .array(z.string())
+        .optional(),
+})
+
+export type UpdateTripDto = z.infer<typeof updateTripSchema>;

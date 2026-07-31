ffb110d23f6b60f827767f665cddbe5924865a21
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-08-01 00:19:53 +0530

    fix(core): preserve typed route params in asyncHandler

--- backend/src/core/errors/async-handler.ts
+++ backend/src/core/errors/async-handler.ts
@@ -1,6 +1,14 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
+import { ParamsDictionary } from "express-serve-static-core";
+import { ParsedQs } from "qs";
 
 export const asyncHandler =
-  (handler: (req: Request, res: Response, next: NextFunction) => any): RequestHandler =>
+  <P = ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = ParsedQs>(
+    handler: (
+      req: Request<P, ResBody, ReqBody, ReqQuery>,
+      res: Response<ResBody>,
+      next: NextFunction
+    ) => any
+  ): RequestHandler<P, ResBody, ReqBody, ReqQuery> =>
   (req, res, next) =>
     Promise.resolve(handler(req, res, next)).catch(next);
